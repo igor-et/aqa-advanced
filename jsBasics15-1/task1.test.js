9ac5550bbb64041375c73519d379e093c2332e52
igor <igor.etkalo@flexaspect.com>
2026-03-11 14:48:12 +0200

    Add task1 and task2 for homework 15-1

--- jsBasics15-1/task1.test.js
+++ jsBasics15-1/task1.test.js
@@ -1,24 +1,24 @@
 const axios = require('axios');
 
 async function getWrongUrl() {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/wrongurl"
-    );
+    try {
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/wrongurl"
+        );
 
-    return response.data;
-  } catch (error) {
-    return error.response.status;
-  }
+        return response.data;
+    } catch (error) {
+        return error.response.status;
+    }
 }
 
 describe("Error handling tests", () => {
 
-  test("should return 404 for wrong endpoint", async () => {
-    const result = await getWrongUrl();
+    test("should return 404 for wrong URL", async () => {
+        const result = await getWrongUrl();
 
-    expect(result).toBe(404);
-    console.log("check what we have in result:",result);
-  });
+        expect(result).toBe(404);
+        console.log("check what we have in result:", result);
+    });
 
 });
